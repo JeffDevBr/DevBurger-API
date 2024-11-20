9bcd38d5c43186a6c91bb84cbbb1de72f760d0b3
Jefferson <jeff.dev.br@gmail.com>
2024-11-20 20:06:14 -0300

    criando controller do usuario

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,17 +1,8 @@
 import { Router } from 'express'
-import {v4} from 'uuid'
-import User from '../app/models/User.js'
+import UserController from '../app/controllers/UserController.js'
 
 const userRoutes = new Router()
 
-userRoutes.get('/', async (req, res) => {
-    const user = await User.create({
-        id: v4(),
-        name: "Jefferson",
-        email: "jeffersosn@email.com",
-        password_hash: "asdfasdf",
-    })
-    return req.status(200).json({message: "user"})
-})
+userRoutes.post('/users', UserController.store )
 
 export default userRoutes
