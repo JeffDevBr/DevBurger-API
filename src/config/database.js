cae34a595ad7e5d5fcdf423deca224bbd93b8513
Jefferson <jeff.dev.br@gmail.com>
2024-11-20 14:56:53 -0300

    instalando a ORM e configurando nosso banco de dados

---
+++ src/config/database.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const config = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+  },
+}
+
+export default config
